68c2ab29217429517e042e00ade865e497ceab2d
Jason Im <bluesky1261@gmail.com>
2022-05-15 20:06:27 +0900

    APIConnector 작성

--- Sources/APIConnector/APIConnector.swift
+++ Sources/APIConnector/APIConnector.swift
@@ -1,6 +1,30 @@
-public struct APIConnector {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import Alamofire
 
-    public init() {
+public struct APIConnector {
+    private let configuration: URLSessionConfiguration
+    private(set) var validStatusCode: Range<Int> = 200..<400
+    
+    public private(set) var session: Session
+    public static let shared: APIConnector = APIConnector()
+
+    public init(configuration: URLSessionConfiguration? = nil) {
+        if let configuration = configuration {
+            self.configuration = configuration
+            self.session = Session(configuration: configuration)
+        } else {
+            self.configuration = URLSessionConfiguration.default
+            self.configuration.headers = [
+                .defaultAcceptEncoding,
+                .defaultAcceptLanguage,
+                .defaultUserAgent
+            ]
+            
+            self.session = Session(configuration: self.configuration)
+        }
+    }
+    
+    public func cancelAllRequests() {
+        session.cancelAllRequests()
     }
 }
