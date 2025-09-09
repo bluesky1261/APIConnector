abea29b766fdabdcaf94c99ac8e93c563082471e
John.f <John.f@kakaoinsurecorp.com>
2025-09-09 18:29:43 +0900

    Download API를 APIResource 이외에도 호출 가능하도록 기능 개선

--- Sources/APIConnector/APIConnector/Download/APIConnector+Download+Concurrency.swift
+++ Sources/APIConnector/APIConnector/Download/APIConnector+Download+Concurrency.swift
@@ -9,10 +9,23 @@ import Foundation
 import Alamofire
 
 extension APIConnector {
+    public func donwload<Parameters>(remoteUrl: URL,
+                                     headers: HTTPHeaders? = nil,
+                                     to destination: DownloadRequest.Destination? = nil,
+                                     parameters: Parameters,
+                                     encoder: ParameterEncoder = JSONParameterEncoder.default) async throws -> URL where Parameters: Encodable {
+        return try await serializingDownload(remoteUrl: remoteUrl,
+                                             headers: headers,
+                                             destination: destination,
+                                             parameters: parameters,
+                                             encoder: encoder)
+        .value(statusCode: configuration.validStatusCode)
+    }
+    
     public func download<Parameters>(resource: any APIResource,
-                              to destination: DownloadRequest.Destination? = nil,
-                              parameters: Parameters,
-                              encoder: ParameterEncoder = JSONParameterEncoder.default) async throws -> URL where Parameters: Encodable {
+                                     to destination: DownloadRequest.Destination? = nil,
+                                     parameters: Parameters,
+                                     encoder: ParameterEncoder = JSONParameterEncoder.default) async throws -> URL where Parameters: Encodable {
         return try await serializingDownload(resource: resource,
                                              destination: destination,
                                              parameters: parameters,
@@ -21,8 +34,8 @@ extension APIConnector {
     }
     
     public func download(resource: APIResource,
-                  to destination: DownloadRequest.Destination? = nil,
-                  encoder: ParameterEncoder = JSONParameterEncoder.default) async throws -> URL {
+                         to destination: DownloadRequest.Destination? = nil,
+                         encoder: ParameterEncoder = JSONParameterEncoder.default) async throws -> URL {
         let parameters = resource.httpMethod == .get ? nil : EmptyParameters()
         
         return try await serializingDownload(resource: resource,
@@ -41,6 +54,16 @@ extension APIConnector {
                                                      encoder: ParameterEncoder = JSONParameterEncoder.default) -> DownloadTask<URL> where Parameters: Encodable {
         return makeDownloadRequest(resource: resource, destination: destination, parameters: parameters, encoder: encoder)
             .validate(retriableStatusCode: (configuration.retriableStatusCode), resource: resource, validator: self.validator)
+            .serializingDownloadedFileURL()
+    }
+    
+    fileprivate func serializingDownload<Parameters>(remoteUrl: URL,
+                                                     headers: HTTPHeaders? = nil,
+                                                     destination: DownloadRequest.Destination? = nil,
+                                                     parameters: Parameters?,
+                                                     encoder: ParameterEncoder = URLEncodedFormParameterEncoder.default) -> DownloadTask<URL> where Parameters: Encodable {
+        return makeDownloadRequest(remoteUrl: remoteUrl, headers: headers, destination: destination, parameters: parameters, encoder: encoder)
+            .validate(retriableStatusCode: (configuration.retriableStatusCode), validator: self.validator)
             .serializingDownloadedFileURL()
     }
 }
--- Sources/APIConnector/APIConnector/Download/APIConnector+Download+MakeRequest.swift
+++ Sources/APIConnector/APIConnector/Download/APIConnector+Download+MakeRequest.swift
@@ -9,9 +9,9 @@ import Foundation
 
 extension APIConnector {
     internal func makeDownloadRequest<Parameters>(resource: APIResource,
-                                                 destination: DownloadRequest.Destination? = nil,
-                                                 parameters: Parameters?,
-                                                 encoder: ParameterEncoder = JSONParameterEncoder.default) -> DownloadRequest where Parameters: Encodable {
+                                                  destination: DownloadRequest.Destination? = nil,
+                                                  parameters: Parameters?,
+                                                  encoder: ParameterEncoder = JSONParameterEncoder.default) -> DownloadRequest where Parameters: Encodable {
         let fullURL = resource.baseURL.appendingPathComponent(resource.endpoint)
         let headerForRequest = makeRequestHeader(resource: resource)
         
@@ -25,8 +25,33 @@ extension APIConnector {
                                 method: resource.httpMethod,
                                 parameters: parameters,
                                 encoder: parameterEncoder,
-                                headers: headers,
+                                headers: headerForRequest,
                                 to: destination)
     }
+    
+    internal func makeDownloadRequest<Parameters>(remoteUrl: URL,
+                                                  headers: HTTPHeaders? = nil,
+                                                  destination: DownloadRequest.Destination? = nil,
+                                                  parameters: Parameters?,
+                                                  encoder: ParameterEncoder = URLEncodedFormParameterEncoder.default) -> DownloadRequest where Parameters: Encodable {
+        var headerForRequest: HTTPHeaders?
+        if let additionalHeaders = headers {
+            headerForRequest = additionalHeaders
+            self.headers.forEach({ header in
+                /// AdditionalHeader에 정의되어 있는 Header값 중, APIClient에 존재하는 값은 APIClient에 있는 기본 값을 무시한다.
+                guard headerForRequest?.value(for: header.name) == nil else { return }
+                headerForRequest?.add(header)
+            })
+        } else {
+            headerForRequest = self.headers
+        }
+         
+        return session.download(remoteUrl,
+                                method: .get,
+                                parameters: parameters,
+                                encoder: encoder,
+                                headers: headerForRequest,
+                                to: destination)
+     }
 
 }
