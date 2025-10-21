1b8308847cb590eb1089aa8b40ff77b1c9fa5a9d
John.f <John.f@kakaoinsurecorp.com>
2025-10-21 11:23:05 +0900

    APIConnectorConfig 설정 개선

--- Sources/APIConnector/APIConnector/APIConnectorConfig.swift
+++ Sources/APIConnector/APIConnector/APIConnectorConfig.swift
@@ -28,20 +28,97 @@ public protocol APIConnectorConfigurable {
 }
 
 // MARK: AppNetworkConfig - 실제 Network 설정
-final class APIConnectorConfig: APIConnectorConfigurable {
-    var sessionConfiguration: URLSessionConfiguration = URLSessionConfiguration.default
+public final class APIConnectorConfig: APIConnectorConfigurable {
+    public var sessionConfiguration: URLSessionConfiguration = URLSessionConfiguration.default
     
-    let validStatusCode: Range<Int> = 200..<400
-    let retriableStatusCode: Range<Int> = 401..<402
+    public var validStatusCode: Range<Int>
+    public var retriableStatusCode: Range<Int>
     
-    let requestTimeOut: TimeInterval = 10
-    let cachePolicy: URLRequest.CachePolicy = .returnCacheDataElseLoad
-    let cacheMemCapacity = 100_000_000
-    let cacheDiskCapacity = 100_000_000
+    public var requestTimeOut: TimeInterval
+    public var cachePolicy: URLRequest.CachePolicy
+    public var cacheMemCapacity: Int
+    public var cacheDiskCapacity: Int
     
-    let headers: HTTPHeaders = [.defaultAcceptEncoding,
-                                .defaultAcceptLanguage,
-                                .defaultUserAgent]
+    public var headers: HTTPHeaders
+    
+    public init(
+        sessionConfiguration: URLSessionConfiguration = .default,
+        validStatusCode: Range<Int> = 200..<400,
+        retriableStatusCode: Range<Int> = 401..<402,
+        requestTimeOut: TimeInterval = 10,
+        cachePolicy: URLRequest.CachePolicy = .returnCacheDataElseLoad,
+        cacheMemCapacity: Int = 100_000_000,
+        cacheDiskCapacity: Int = 100_000_000,
+        headers: HTTPHeaders = [.defaultAcceptEncoding, .defaultAcceptLanguage, .defaultUserAgent]
+    ) {
+        self.sessionConfiguration = sessionConfiguration
+        self.validStatusCode = validStatusCode
+        self.retriableStatusCode = retriableStatusCode
+        self.requestTimeOut = requestTimeOut
+        self.cachePolicy = cachePolicy
+        self.cacheMemCapacity = cacheMemCapacity
+        self.cacheDiskCapacity = cacheDiskCapacity
+        self.headers = headers
+    }
+}
+
+// MARK: - Fluent Builder (체이닝)
+public extension APIConnectorConfig {
+    @discardableResult
+    func withSessionConfiguration(_ config: URLSessionConfiguration) -> Self {
+        self.sessionConfiguration = config
+        return self
+    }
+
+    @discardableResult
+    func withValidStatusCode(_ range: Range<Int>) -> Self {
+        self.validStatusCode = range
+        return self
+    }
+
+    @discardableResult
+    func withRetriableStatusCode(_ range: Range<Int>) -> Self {
+        self.retriableStatusCode = range
+        return self
+    }
+
+    @discardableResult
+    func withTimeout(_ seconds: TimeInterval) -> Self {
+        self.requestTimeOut = seconds
+        return self
+    }
+
+    @discardableResult
+    func withCachePolicy(_ policy: URLRequest.CachePolicy) -> Self {
+        self.cachePolicy = policy
+        return self
+    }
+
+    @discardableResult
+    func withCache(memory: Int? = nil, disk: Int? = nil) -> Self {
+        if let memory { self.cacheMemCapacity = memory }
+        if let disk { self.cacheDiskCapacity = disk }
+        return self
+    }
+
+    @discardableResult
+    func withHeaders(_ headers: HTTPHeaders) -> Self {
+        self.headers = headers
+        return self
+    }
+
+    /// Header 추가(같은 키가 있으면 교체)
+    @discardableResult
+    func addHeader(name: String, value: String) -> Self {
+        var h = self.headers
+        if h.value(for: name) != nil {
+            h.update(name: name, value: value)
+        } else {
+            h.add(name: name, value: value)
+        }
+        self.headers = h
+        return self
+    }
 }
 
 // MARK: StubAPIConnectorConfig - 테스트용 Network 설정
